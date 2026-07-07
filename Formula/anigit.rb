26fb3cb05cd42013ab69753923ce1333627d0a0d
Thesupersupersigma <thesupersupersigma@gmail.com>
2026-07-07 04:16:50 -0400

    bump to 1.4.8, fix license to GPL-3.0-only

--- Formula/anigit.rb
+++ Formula/anigit.rb
@@ -1,26 +1,26 @@
 class Anigit < Formula
   desc "Git, but for your anime-watching history"
   homepage "https://github.com/TSSS-org/anigit"
-  version "1.4.7"
-  license "MIT"
+  version "1.4.8"
+  license "GPL-3.0-only"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/TSSS-org/anigit/releases/download/v1.4.7/anigit-aarch64-apple-darwin.tar.gz"
-      sha256 "c2810285433a5e0c3b8648f715e606878a61561f8044aa38fe74f773353a3648"
+      url "https://github.com/TSSS-org/anigit/releases/download/v1.4.8/anigit-aarch64-apple-darwin.tar.gz"
+      sha256 "bb39fcc08649ce74ed68c4d6dafde82f961745f5dcfc20fb188b2abbf92436ef"
     else
-      url "https://github.com/TSSS-org/anigit/releases/download/v1.4.7/anigit-x86_64-apple-darwin.tar.gz"
-      sha256 "bfc9363fc9accb01a2229e7cbdad11ff035262f3699e70fcc830aed136265794"
+      url "https://github.com/TSSS-org/anigit/releases/download/v1.4.8/anigit-x86_64-apple-darwin.tar.gz"
+      sha256 "0bf826f9a71327a14bf5660a2c7b919c1304cc4fdd899173ee5202e9ec583313"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/TSSS-org/anigit/releases/download/v1.4.7/anigit-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "486b4fcf48a0fb8e7bbfdbe0fbba35fd356c164d2c3c1bcd831223419bc16a74"
+      url "https://github.com/TSSS-org/anigit/releases/download/v1.4.8/anigit-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "917c8bab600b3a9b1c39e3ae35290880c7cebe171809885f0e1adaee2a01b4a2"
     else
-      url "https://github.com/TSSS-org/anigit/releases/download/v1.4.7/anigit-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "0a409baf44ea10fa6e54c21aacc1f27985dd871b510226c514a9cb51afa90827"
+      url "https://github.com/TSSS-org/anigit/releases/download/v1.4.8/anigit-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "cf60ff28ae1bf46328f1b1277514fc3f662815c848dee6627e48a73ef64dcdac"
     end
   end
 
